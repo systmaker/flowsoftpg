6efafe25fd8e773fb402389e0cab920582622bb5
systmaker <systmaker@hotmail.com>
2017-10-18 16:58:56 -0500

    Commit 2017-10-18 16:58
    Commit implementacion listar en entidad color.html

--- src/main/java/co/com/erp/flowsoftpg/controller/ColorController.java
+++ src/main/java/co/com/erp/flowsoftpg/controller/ColorController.java
@@ -1,5 +1,37 @@
 package co.com.erp.flowsoftpg.controller;
 
-public class ColorController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import co.com.erp.flowsoftpg.entity.Color;
+import co.com.erp.flowsoftpg.service.IColorService;
+
+@Controller
+public class ColorController {
+	
+	@Autowired
+	private IColorService colorService;
+	
+	/*Forma 1 de Listar*/
+	/*
+	@GetMapping ("/color")
+	public ModelAndView init (){
+		ModelAndView view = new ModelAndView ();
+		view.addObject("colores", colorService.listAll());
+		view.addObject("color", new Color());
+		return view;
+	}
+	*/
+	
+	/*Forma 2 de Listar*/
+    @RequestMapping("/color")
+    public String color(Model model) {
+        model.addAttribute("colores", colorService.listAll());
+        return "color";
+    }
+	
 }
