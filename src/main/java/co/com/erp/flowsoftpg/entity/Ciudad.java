cee7b7c756a032e9839ac51dae9a63d686ab0418
systmaker <systmaker@hotmail.com>
2017-10-22 01:08:44 -0500

    Commit 2017-10-22 01:01
    Commit mejoras variedad.html
    Creacion clases Pais, Departamento, Ciudad.

---
+++ src/main/java/co/com/erp/flowsoftpg/entity/Ciudad.java
@@ -0,0 +1,51 @@
+package co.com.erp.flowsoftpg.entity;
+
+public class Ciudad {
+	
+	private Integer id;
+	private String codigo;
+	private String descripcion;
+	private String estado;
+	private Departamento departamento;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
+	
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	public String getEstado() {
+		return estado;
+	}
+	
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	public Departamento getDepartamento() {
+		return departamento;
+	}
+	
+	public void setDepartamento(Departamento departamento) {
+		this.departamento = departamento;
+	}
+		
+}
---
+++ src/main/java/co/com/erp/flowsoftpg/entity/Departamento.java
@@ -0,0 +1,51 @@
+package co.com.erp.flowsoftpg.entity;
+
+public class Departamento {
+	
+	private Integer id;
+	private String codigo;
+	private String descripcion;
+	private String estado;
+	private Pais pais;
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getDescripcion() {
+		return descripcion;
+	}
+	
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	public String getEstado() {
+		return estado;
+	}
+	
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	public Pais getPais() {
+		return pais;
+	}
+	
+	public void setPais(Pais pais) {
+		this.pais = pais;
+	}
+		
+}
