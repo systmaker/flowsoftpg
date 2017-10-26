adc7c3b0f26fcc90f5a3731c3b0432ffdc73a7da
systmaker <systmaker@hotmail.com>
2017-10-26 16:04:14 -0500

    Commit 2017-26-10 16:03
    Commit mejora siembra.html

--- src/main/java/co/com/erp/flowsoftpg/controller/SiembraController.java
+++ src/main/java/co/com/erp/flowsoftpg/controller/SiembraController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import co.com.erp.flowsoftpg.entity.Siembra;
 import co.com.erp.flowsoftpg.entity.Variedad;
 import co.com.erp.flowsoftpg.service.ICompaniaService;
+import co.com.erp.flowsoftpg.service.IInvernaderoService;
 import co.com.erp.flowsoftpg.service.ILocalizacionService;
 import co.com.erp.flowsoftpg.service.ISiembraService;
 
@@ -25,10 +26,14 @@ public class SiembraController {
 	@Autowired
 	private ILocalizacionService localizacionService;
 	
+	@Autowired
+	private IInvernaderoService invernaderoService;	
+	
 	@RequestMapping("/siembra")
 	public String siembra(@ModelAttribute ("siembra") Siembra siembra, Model model){
 		model.addAttribute("companias", companiaService.listAll());
 		model.addAttribute("localizaciones", localizacionService.listAll());
+		model.addAttribute("invernaderos", invernaderoService.listAll());
 		return "siembra";
 	}
 	
