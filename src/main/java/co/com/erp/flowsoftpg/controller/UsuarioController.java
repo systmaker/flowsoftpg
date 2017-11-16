d85e92aea2f2e9b0d0ccd02b36a831d42c4911a7
systmaker <systmaker@hotmail.com>
2017-11-16 18:59:25 -0500

    Commit 2017-11-16 18:58
    Commit correccion clase Usuario.

--- src/main/java/co/com/erp/flowsoftpg/controller/UsuarioController.java
+++ src/main/java/co/com/erp/flowsoftpg/controller/UsuarioController.java
@@ -28,7 +28,7 @@ public class UsuarioController {
 	@RequestMapping("/usuario")
 	public String usuario(@ModelAttribute("usuario") Usuario usuario, Model model){
 		model.addAttribute("usuarios", usuarioService.listAll());
-		return "redirect:/usuario";
+		return "usuario";
 	}
 	
 	@RequestMapping("/")
